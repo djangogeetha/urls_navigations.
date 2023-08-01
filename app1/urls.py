eddac5b2583dceb05bea0eaf5f4cc9c7e63edfaf
djangogeetha <geethaputtur369@gmail.com>
2023-08-01 11:56:08 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from  django.urls import path
+from app1.views import *
+
+
+urlpatterns=[
+    path('geetha/',geetha,name='geetha'),
+]
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def harshi(request):
+    return render(request,'harshi.html')
+
